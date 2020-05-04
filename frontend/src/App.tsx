d793569fd10159a6e983638d4b5545dd30508336
iyzana <16743652+iyzana@users.noreply.github.com>
2020-05-04 02:01:04 +0200

    simplify play/pause filtering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,24 +3,14 @@ import "./App.css";
 import YtEmbed from "./YtEmbed";
 import YouTube from "react-youtube";
 
+const ws = new WebSocket("ws://localhost:4567/room");
+
 function App() {
   const [player, setPlayer] = useState<any | null>(null);
-  const [ws, setWs] = useState<WebSocket | null>(null);
-  const [expectedEvents, setExpectedEvents] = useState<number[]>([]);
 
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:4567/room");
-    setWs(ws);
-    return () => ws.close();
-  }, []);
-
-  useEffect(() => {
-    if (ws === null) {
-      return;
-    }
     ws.onopen = () => {
       const path = window.location.pathname;
-      console.log(path);
       if (path === "" || path === "/") {
         ws.send("create");
       } else {
@@ -31,9 +21,6 @@ function App() {
   }, [ws]);
 
   useEffect(() => {
-    if (ws === null) {
-      return;
-    }
     ws.onclose = () => {
       console.log("disconnected");
     };
@@ -46,19 +33,11 @@ function App() {
       } else if (msg === "invalid command") {
         window.location.href = "/";
       } else if (msg === "play") {
-        if (
-          player.getPlayerState() !== YouTube.PlayerState.PLAYING ||
-          expectedEvents.indexOf(YouTube.PlayerState.PAUSED) !== -1
-        ) {
-          setExpectedEvents((ev) => [...ev, YouTube.PlayerState.PLAYING]);
+        if (player.getPlayerState() !== YouTube.PlayerState.PLAYING) {
           player?.playVideo();
         }
       } else if (msg === "pause") {
-        if (
-          player.getPlayerState() !== YouTube.PlayerState.PAUSED ||
-          expectedEvents.indexOf(YouTube.PlayerState.PLAYING) !== -1
-        ) {
-          setExpectedEvents((ev) => [...ev, YouTube.PlayerState.PAUSED]);
+        if (player.getPlayerState() !== YouTube.PlayerState.PAUSED) {
           player?.pauseVideo();
         }
       }
@@ -67,17 +46,12 @@ function App() {
 
   const onStateChange = () => {
     console.log("player state changed to " + player.getPlayerState());
-    const expectedTransition = expectedEvents[0];
-    if (player.getPlayerState() !== expectedTransition) {
-      if (player.getPlayerState() === YouTube.PlayerState.PAUSED) {
-        console.log("sending pause");
-        ws?.send("pause");
-      } else if (player.getPlayerState() === YouTube.PlayerState.PLAYING) {
-        console.log("sending play");
-        ws?.send("play");
-      }
-    } else {
-      setExpectedEvents((ev) => ev.slice(1));
+    if (player.getPlayerState() === YouTube.PlayerState.PAUSED) {
+      console.log("sending pause");
+      ws.send("pause");
+    } else if (player.getPlayerState() === YouTube.PlayerState.PLAYING) {
+      console.log("sending play");
+      ws.send("play");
     }
   };
   const ready = (player: any) => {
@@ -97,4 +71,3 @@ function App() {
 }
 
 export default App;
-
--- frontend/src/YtEmbed.tsx
+++ frontend/src/YtEmbed.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import "./YtEmbed.css";
 import YouTube, { Options } from "react-youtube";
 
