742e2dc716b7ee3c1bf41b1fefd263ca6ddb82c5
iyzana <16743652+iyzana@users.noreply.github.com>
2025-07-15 16:53:51 +0200

    chore: update dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     kotlin("jvm") version "2.1.0"
 
     application
-    id("com.gradleup.shadow") version "8.3.5"
-    id("io.gitlab.arturbosch.detekt") version "1.23.7"
+    id("com.gradleup.shadow") version "8.3.8"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
 }
 
 kotlin {
@@ -18,13 +18,13 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("io.javalin:javalin:6.3.0")
-    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("io.javalin:javalin:6.7.0")
+    implementation("com.google.code.gson:gson:2.13.1")
 
-    implementation("ch.qos.logback:logback-classic:1.5.12")
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.6")
 
-    implementation("com.mohamedrejeb.ksoup:ksoup-html:0.4.1")
+    implementation("com.mohamedrejeb.ksoup:ksoup-html:0.6.0")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
